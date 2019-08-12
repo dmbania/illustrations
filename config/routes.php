8428e6f5a32e03b95989e73044c67d419c066ef2
David Bania <david.bania@foresters.com>
2019-08-12 12:58:41 -0400

    Comment out middle where include

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 use ForestersFinancial\FliacIllustrations\Http\Action;
-use ForestersFinancial\FliacIllustrations\Http\Middleware;
+// use ForestersFinancial\FliacIllustrations\Http\Middleware;
 
 // $app->get('/user/login', Action\User\Login::class);
 // $app->get('/user/logout', Action\User\Logout::class);
