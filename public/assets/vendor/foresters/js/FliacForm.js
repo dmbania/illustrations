9fdcb86772f9aedc489230c7dfc785df50c8712e
David Bania <david.bania@foresters.com>
2019-08-15 14:44:47 -0400

    Initial code to show/hide form elements
    We'll show or hide elements based on the product prefix from the
    dropdown box. We'll add an event on the drop down to determine which
    form fields and menu items to show/hide.
    
    We'll default to ISP3, for now.

--- public/assets/vendor/foresters/js/FliacForm.js
+++ public/assets/vendor/foresters/js/FliacForm.js
@@ -1,78 +1,108 @@
-import { FormCollector } from '/assets/vendor/foresters/js/FormCollector.js';
 import { DataStreamProxy } from '/assets/vendor/foresters/js/DataStreamProxy.js';
 
 export class FliacForm {
-  constructor(illInputForm) {
-    this.initialize(illInputForm);
-  }
 
-  initialize(illInputForm) {
-    // const allInputs = illInputForm.querySelectorAll('input[type=text], input[type=number], input[type=radio], select')
-    const theForm = illInputForm.querySelectorAll('form > section');
+    constructor(illInputForm) {
+        this.selected_product_type = 'ISP3';
+        this.show('.isp3-info');
+        this.hide('.iswl-info');
+        this.initialize(illInputForm);
+    }
 
-    // allInputs.forEach(input_item => {
-    //   input_item.addEventListener('change', evt => {
-    //     const val = evt.target.value
+    initialize(illInputForm) {
+        // const allInputs = illInputForm.querySelectorAll('input[type=text], input[type=number], input[type=radio], select')
+        const theForm = illInputForm.querySelectorAll('form > section');
 
-    //     const dataStream = new DataStreamProxy(illInputForm)
-    //     dataStream.update()
-    //   })
-    // })
-    theForm.forEach(input_item => {
-      input_item.addEventListener('change', evt => {
-        const val = evt.target.value;
+        // allInputs.forEach(input_item => {
+        //   input_item.addEventListener('change', evt => {
+        //     const val = evt.target.value
 
-        const dataStream = new DataStreamProxy(illInputForm);
-        dataStream.update();
-      });
-    });
+        //     const dataStream = new DataStreamProxy(illInputForm)
+        //     dataStream.update()
+        //   })
+        // })
+        theForm.forEach(input_item => {
+            input_item.addEventListener('change', evt => {
+                const val = evt.target.value;
 
-    const calculateEvent = new CustomEvent('calculate', {
-      bubbles: true,
-      detail: { text: () => 'test' }
-    });
+                const dataStream = new DataStreamProxy(illInputForm);
+                dataStream.update();
+            });
+        });
 
-    const allCheckboxes = illInputForm.querySelectorAll('input[type=checkbox]');
+        const calculateEvent = new CustomEvent('calculate', {
+            bubbles: true,
+            detail: {
+                text: () => 'test'
+            }
+        });
 
-    allCheckboxes.forEach(checkbox_input => {
-      checkbox_input.addEventListener('click', evt => {
-        evt.target.dispatchEvent(calculateEvent);
-      });
-    });
+        const allCheckboxes = illInputForm.querySelectorAll('input[type=checkbox]');
 
-    illInputForm.querySelectorAll('.annual-premium').forEach(formDiv => {
-      formDiv.addEventListener('calculate', e => console.log(e.detail.text()));
-    });
+        allCheckboxes.forEach(checkbox_input => {
+            checkbox_input.addEventListener('click', evt => {
+                evt.target.dispatchEvent(calculateEvent);
+            });
+        });
 
-    illInputForm.querySelector('.select-product').addEventListener('change', evt => {
-      let prod_prefix = evt.target.value.substring(0, 4);
+        illInputForm.querySelectorAll('.annual-premium').forEach(formDiv => {
+            formDiv.addEventListener('calculate', e => console.log(e.detail.text()));
+        });
 
-      switch (prod_prefix) {
-        case 'ISP3':
-          console.log('show ISP3');
-          console.log('hide ISWL');
-          break;
-        case 'ISWL':
-          console.log('show ISWL');
-          console.log('hide ISP3');
-          break;
-      }
-    });
+        illInputForm.querySelector('.select-product').addEventListener('change', evt => {
+            let prod_prefix = evt.target.value.substring(0, 4);
 
-    // illInputForm.querySelectorAll('.dollar-amount').forEach(dollarInput => {
-    //     dollarInput.addEventListener('keypress', dollarIn => {
-    //         let dollar_input = dollarIn.target.value
+            if (this.changedProduct(prod_prefix)) {
+                switch (prod_prefix) {
+                    case 'ISP3':
+                        this.show('.isp3-info');
+                        this.hide('.iswl-info');
+                        break;
+                    case 'ISWL':
+                        this.show('.iswl-info');
+                        this.hide('.isp3-info');
+                        break;
+                }
 
-    //         // if (isNaN(dollar_input)) {
-    //         //     dollar_input = 0
-    //         // }
+                this.setProductType(prod_prefix);
+            }
+        });
 
-    //         dollarIn.target.value = new Intl.NumberFormat('en-US', {
-    //             minimumFractionDigits: 2,
-    //             maximumFractionDigits: 2
-    //         }).format(dollar_input)
+        // illInputForm.querySelectorAll('.dollar-amount').forEach(dollarInput => {
+        //     dollarInput.addEventListener('keypress', dollarIn => {
+        //         let dollar_input = dollarIn.target.value
 
-    //     })
-    // })
-  }
+        //         // if (isNaN(dollar_input)) {
+        //         //     dollar_input = 0
+        //         // }
+
+        //         dollarIn.target.value = new Intl.NumberFormat('en-US', {
+        //             minimumFractionDigits: 2,
+        //             maximumFractionDigits: 2
+        //         }).format(dollar_input)
+
+        //     })
+        // })
+    }
+
+    hide(hide_class) {
+        document.querySelectorAll(hide_class).forEach(ele => {
+            ele.classList.add('ff-hidden');
+        });
+    }
+
+    show(show_class) {
+        document.querySelectorAll(show_class).forEach(ele => {
+            ele.classList.remove('ff-hidden');
+        });
+    }
+
+    changedProduct(prod_prefix) {
+        return prod_prefix !== this.selected_product_type;
+    }
+
+    setProductType(prod_prefix) {
+        this.selected_product_type = prod_prefix;
+    }
+
 }
