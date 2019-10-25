fe78ef9bd7af98901a8c5fd62a79fe26799a554d
David Bania <david.bania@foresters.com>
2019-10-25 13:40:13 -0400

    Build table listign of agents

--- public/assets/vendor/foresters/js/AgentIndex.js
+++ public/assets/vendor/foresters/js/AgentIndex.js
@@ -1,57 +1,9 @@
 import { AgentProfileManager } from '/assets/vendor/foresters/js/AgentProfileManager.js';
+import { AgentProfileTable } from '/assets/vendor/foresters/js/AgentProfileTable.js';
 
 const agentStorage = window.localStorage;
 const APM = new AgentProfileManager(agentStorage);
-const profile_form = document.querySelector('.agent-profile-form');
-
 // APM.showAgents()
 
 const agents = APM.getAgents();
-
-console.log(agents);
-
-// let agent2 = {
-//     "agent-name" : "agent One",
-//     "agent-title" : "Agent Suprem",
-//     "agent-company" : "Foresters Financial",
-//     "agent-state" : "NJ",
-//     "agent-website" : ""
-// }
-
-// APM.saveAgent({
-//     "agent-name" : "agent One",
-//     "agent-title" : "Agent Suprem",
-//     "agent-company" : "Foresters Financial",
-//     "agent-state" : "NJ",
-//     "agent-website" : ""
-// });
-
-// APM.saveAgent({
-//     "agent-name" : "agent Three",
-//     "agent-title" : "Agent Supreme",
-//     "agent-company" : "Foresters Financial",
-//     "agent-state" : "NJ",
-//     "agent-website" : ""
-// });
-
-// APM.showAgents()
-
-// APM.saveAgent({
-//     "agent-name" : "Agent One",
-//     "agent-title" : "Agent Supreme",
-//     "agent-company" : "Foresters Financial",
-//     "agent-state" : "NJ",
-//     "agent-website" : ""
-// });
-
-// APM.saveAgent({
-//     "agent-name" : "Agent Four",
-//     "agent-title" : "Agent Lackey",
-//     "agent-company" : "Foresters Financial",
-//     "agent-state" : "NY",
-//     "agent-website" : ""
-// });
-
-// APM.showAgents()
-
-// APM.clear()
+const agentTable = new AgentProfileTable(agents, document.querySelector('.agent-table'));
---
+++ public/assets/vendor/foresters/js/AgentProfileTable.js
@@ -0,0 +1,36 @@
+export class AgentProfileTable {
+    constructor(agents, table) {
+        this.agents = agents || null;
+        this.table = table || null;
+
+        this.getTableParts();
+        this.showHideNoAgents();
+        this.buildTable();
+    }
+
+    getTableParts() {
+        this.noAgentMessage = this.table.querySelector('.no-agents');
+        this.tableBody = this.table.querySelector('tbody');
+    }
+
+    showHideNoAgents() {
+        if (this.agents !== null) {
+            this.noAgentMessage.classList.add('ff-hidden');
+        }
+    }
+
+    buildTable() {
+        Object.keys(this.agents).forEach(agentId => {
+            let oneAgent = this.agents[agentId];
+            let newRow = document.createElement('tr');
+            let agentInformation = `
+                <td><a href="/agent/profile/${agentId}">${oneAgent['agent-name']}</a></td>
+                <td>${oneAgent['agent-company']}</td>
+                <td>${oneAgent['agent-state']}</td>
+            `;
+            newRow.innerHTML = agentInformation;
+
+            this.tableBody.appendChild(newRow);
+        });
+    }
+}
