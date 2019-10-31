2b8fc37d1911c0bdde19437ab10dafd4edb53192
David Bania <david.bania@foresters.com>
2019-10-31 12:35:04 -0400

    Add an add button

--- public/assets/vendor/foresters/js-src/AgentIndex.js
+++ public/assets/vendor/foresters/js-src/AgentIndex.js
@@ -3,7 +3,25 @@ import { AgentProfileTable } from '/assets/vendor/foresters/js/AgentProfileTable
 
 const agentStorage = window.localStorage;
 const APM = new AgentProfileManager(agentStorage)
-// APM.showAgents()
-
 const agents = APM.getAgents()
 const agentTable = new AgentProfileTable(agents, document.querySelector('.agent-table'))
+
+// APM.clear()
+// APM.showAgents()
+// APM.addAgent({
+//     "agent-name" : "Agent One",
+//     "agent-uid" : "",
+//     "agent-title" : "Agent Smith",
+//     "agent-company" : "Foresters Financial",
+//     "agent-address-1" : "#3445 High Street",
+//     "agent-address-2" : "",
+//     "agent-city" : "Newark",
+//     "agent-zip" : "07928",
+//     "agent-state" : "NJ",
+//     "agent-website" : "http://www.example.com"
+// })
+
+const add_button = document.querySelector('.add-agent-button').addEventListener('click', evt => {
+    window.location.href = '/agent/profile'
+    evt.preventDefault();
+})
