228ee1c5681a4b614c12ded5dc7305189b16797a
David Bania <david.bania@foresters.com>
2019-10-31 10:38:57 -0400

    Make fix fixer fixes "

--- src/Http/Action/Agent/Index.php
+++ src/Http/Action/Agent/Index.php
@@ -2,8 +2,6 @@
 
 namespace ForestersFinancial\FliacIllustrations\Http\Action\Agent;
 
-use ForestersFinancial\RepNet\Http\Message\JsonResponse;
-use ForestersFinancial\RepNet\Http\Message\XmlResponse;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
@@ -20,6 +18,7 @@ class Index
     public function __invoke(Request $request, Response $response)
     {
         $vars = [];
+
         return $this->twig->render($response, 'agent/index.twig', $vars);
     }
 }
