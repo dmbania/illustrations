baabe136860728ebc820a83651e3a0921a111236
David Bania <david.bania@foresters.com>
2019-11-01 13:47:56 -0400

    Replace missing FliacForm

--- public/assets/vendor/foresters/js-src/FliacIndex.js
+++ public/assets/vendor/foresters/js-src/FliacIndex.js
@@ -1,5 +1,7 @@
+import { FliacForm } from '/assets/vendor/foresters/js/FliacForm.js'
 import { RunIllustrationButton } from '/assets/vendor/foresters/js/RunIllustrationButton.js'
 
 const fliac_form = document.querySelector('.illustration-form')
+const IllustrationForm = new FliacForm(fliac_form)
 const run_button = document.querySelector('.run-illustration-button')
 const RunButton = new RunIllustrationButton(run_button, fliac_form)
